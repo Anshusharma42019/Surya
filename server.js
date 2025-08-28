234bf9ca90e1acc76773e1332641ee9b64e05eca
Anshu sharma <156065826+Anshusharma42019@users.noreply.github.com>
2025-08-28 13:28:37 +0530

    ZX

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://surya-two.vercel.app",
+  "https://surya-psi.vercel.app/",
 ];
 app.use(
   cors({
