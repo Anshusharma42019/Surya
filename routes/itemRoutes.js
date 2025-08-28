0944072fee61882ab4e43493aa0ddc8018c84e72
Anshu sharma <156065826+Anshusharma42019@users.noreply.github.com>
2025-08-28 13:27:01 +0530

    xz

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,11 +4,11 @@ import { createItem, getItems, getItem, updateItem, deleteItem, getUnitTypes } f
 const router = express.Router();
 
 router.post("/", createItem); 
-router.get("/", getItems);
+
+router.get("/unit-types", getUnitTypes);
 router.get("/:id", getItem);
 router.put("/:id", updateItem);
 router.delete("/:id", deleteItem);
-router.get("/unit-types", getUnitTypes);
 
 
 export default router;
